8bc7e47c85c2f20f159cac1f8a88b906d3db2aef
= <=>
2024-03-07 14:20:23 +0700

    seeder

---
+++ app/Models/UserModel.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserModel extends Model
+{
+    use HasFactory;
+
+
+    protected $tabel = 'm_user';
+    protected $primaryKey = 'user_id';
+
+  protected $fillable =['level_id', 'username', 'nama', 'password'];
+
+}
+
+
+
