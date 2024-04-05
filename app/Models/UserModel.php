4f41222e3f5f1d95ce4fa00ffea4af4009641244
= <=>
2024-04-05 08:45:59 +0700

    praktikum 4 jb 7

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -8,20 +8,20 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class UserModel extends Model
 {
-  
+
 
   use HasFactory;
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
 
     /**
-     * 
+     *
      * @var array
      */
 
   protected $fillable =['level_id', 'username', 'nama', 'password'];
 
-  public function level()
+  public function level(): BelongsTo
   {
     return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
   }
--- resources/views/layout/breadcrumb.blade.php
+++ resources/views/layout/breadcrumb.blade.php
@@ -1,7 +1,7 @@
 <section class="content-header">
     <div class="container-fluid">
       <div class="row mb-2">
-        <div class="col-sm-6"><h1>{{ $breadcrumb->tittle}}</h1></div>
+        <div class="col-sm-6"><h1>{{ $breadcrumb->title}}</h1></div>
         <div class="col-sm-6">
           <ol class="breadcrumb float-sm-right">
             @foreach ($breadcrumb->list as $key => $value)
